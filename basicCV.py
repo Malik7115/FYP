2cec8f00990630a26600e7e47389be746110234e
Malik7115 <ibmalik96@yahoo.com>
2018-08-07 12:08:40 +0500

    Update and rename imgproc.py to basicCV.py

--- basicCV.py
+++ basicCV.py
@@ -18,11 +18,19 @@ def nothing(x):
 print('start prog')
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cv2.namedWindow('orig',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('orig', 600,600)
 cv2.namedWindow('th',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('th', 600,600)
+cv2.namedWindow('webcam',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('webcam', 600,600)
+cv2.namedWindow('H',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('H', 600,600)
+cv2.namedWindow('S',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('S', 600,600)
+cv2.namedWindow('V',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('V', 600,600)
 
 
 
@@ -33,9 +41,26 @@ print (img.shape)
 cv2.createTrackbar('low','orig',0,255,nothing)
 cv2.createTrackbar('high','orig',0,255,nothing)
 
+cv2.createTrackbar('lh','H',0,255,nothing)
+cv2.createTrackbar('hh','H',0,255,nothing)
+
+cv2.createTrackbar('ls','S',0,255,nothing)
+cv2.createTrackbar('hs','S',0,255,nothing)
+
+cv2.createTrackbar('lv','V',0,255,nothing)
+cv2.createTrackbar('hv','V',0,255,nothing)
+
 
 while(1):
     cv2.imshow('orig',img)
+    ret,frame = cap.read()
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV_FULL)
+    h,s,v = cv2.split(hsv)
+    
+    cv2.imshow('webcam',frame)
+    cv2.imshow('H',h)
+    cv2.imshow('S',s)
+    cv2.imshow('V',v)
     
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
@@ -43,8 +68,11 @@ while(1):
     l = cv2.getTrackbarPos('low','orig')
     h = cv2.getTrackbarPos('high','orig')
     
-#     ret,threshold = cv2.THRESH_BINARY(img,l,h,cv2.THRESH_BINARY)
-#     cv2.imshow('th',threshold)
+    
+    
+    ret,threshold = cv2.threshold(img,l,h,cv2.THRESH_BINARY)
+    cv2.imshow('th',threshold)
+    
     
 cv2.destroyAllWindows()
     
