e42b26f30524a5a16e4e9b49a8eac9893679f741
Malik7115 <ibmalik96@yahoo.com>
2018-08-08 15:09:26 +0500

    Update HSVColourFilterWithSlideres.py

--- HSVColourFilterWithSlideres.py
+++ HSVColourFilterWithSlideres.py
@@ -15,15 +15,21 @@ cv2.createTrackbar('US','sliders',0,255,nothing)
 cv2.createTrackbar('LV','sliders',0,255,nothing)
 cv2.createTrackbar('UV','sliders',0,255,nothing)
 #################print RGB ranges
-
+kernel = np.ones((5,5),np.uint8)
 
 
 while(1):
     _, frame = cap.read()
+    
+    
+    
     hsv = cv2.cvtColor(balls, cv2.COLOR_BGR2HSV)
 #     
-    h,s,v = cv2.split(balls)
-    cv2.imshow('balls',balls)
+    h,s,v = cv2.split(frame)
+    
+    frame = cv2.resize(frame,(340,220))
+    
+    cv2.imshow('orig',frame)
     
     
     LH = cv2.getTrackbarPos('LH','sliders')
@@ -33,22 +39,30 @@ while(1):
     LV = cv2.getTrackbarPos('LV','sliders')
     UV = cv2.getTrackbarPos('UV','sliders')
     
-    h = cv2.inRange(h,LH,UH)
-    s = cv2.inRange(s,LS,US)
-    v = cv2.inRange(v,LV,UV)
+    h = cv2.inRange(h,133,239)
+    s = cv2.inRange(s,0,255)
+    v = cv2.inRange(v,23,98)
     
     
     
-    cv2.imshow('H',h)
-    cv2.imshow('S',s)
-    cv2.imshow('V',v)
+    
     
     total = cv2.bitwise_and(h,s)
     total = cv2.bitwise_and(total,v)
+    opening = cv2.morphologyEx(total, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(total, cv2.MORPH_CLOSE, kernel)
+    opening = cv2.resize(opening,(340,220))
+    total = cv2.resize(total,(340,220))
+    closing = cv2.resize(closing,(340,220))
     
-    cv2.imshow('res',total)
     
+    _, contours, _ = cv2.findContours(opening, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow('op',opening)
+    cv2.imshow('clos',closing)
+    cv2.drawContours(frame,contours,-1,(0,0,255),1)
     
+    res = cv2.bitwise_and(frame,frame,mask=opening)
+    cv2.imshow('res',res)
 
 #     threshH = cv2.inRange(h,LH,UH)
 #     
@@ -58,7 +72,7 @@ while(1):
 #     mask = cv2.inRange(hsv, lower_red, upper_red)
 #     res = cv2.bitwise_and(frame,frame, mask= mask)
 # 
-    cv2.imshow('frame',frame)
+    
 #     cv2.imshow('H',threshH)
 #     cv2.imshow('res',res)
 #     
@@ -67,9 +81,7 @@ while(1):
         break
     
             
-im2,contours,heir = cv2.findContours(total,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(balls,contours,-1,(255,0,0),2)
-cv2.imshow('balls',balls)
-cv2.waitKey(0)
+
+
 cv2.destroyAllWindows()
 cap.release()
