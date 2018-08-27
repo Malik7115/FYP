ac94f1f3387149c629aa241c68cf386aa956db6a
Malik7115 <ibmalik96@yahoo.com>
2018-08-27 11:06:59 +0500

    Update BGR_based_boundedRect_on_largest.py

--- BGR_based_boundedRect_on_largest.py
+++ BGR_based_boundedRect_on_largest.py
@@ -1,68 +1,82 @@
 import cv2
 import numpy as np
 
+
 def nothing(x):
     pass 
 
+
 cap = cv2.VideoCapture(1)
+
 cv2.namedWindow('sliders')
-balls = cv2.imread('balls2.jpeg',1)
+cv2.createTrackbar('LH','sliders',150,255,nothing)
 
-cv2.createTrackbar('LH','sliders',0,255,nothing)
-cv2.createTrackbar('UH','sliders',0,255,nothing)
-cv2.createTrackbar('LS','sliders',0,255,nothing)
-cv2.createTrackbar('US','sliders',0,255,nothing)
-cv2.createTrackbar('LV','sliders',0,255,nothing)
-cv2.createTrackbar('UV','sliders',0,255,nothing)
-#################print RGB ranges
-kernel = np.ones((5,5),np.uint8)
-
+check = 0
 
 while(1):
-    _, frame = cap.read()
-    
-    
-    
-    
-    frame = cv2.resize(frame,(340,220))
-    
-    
-    
-    
+    ret,frame = cap.read()
     LH = cv2.getTrackbarPos('LH','sliders')
-    UH = cv2.getTrackbarPos('UH','sliders')
-    LS = cv2.getTrackbarPos('LS','sliders')
-    US = cv2.getTrackbarPos('US','sliders')
-    LV = cv2.getTrackbarPos('LV','sliders')
-    UV = cv2.getTrackbarPos('UV','sliders')
-    
     b,g,r = cv2.split(frame)
-    _,b = cv2.threshold(b,UH,255,cv2.THRESH_BINARY_INV)
-    _,r = cv2.threshold(r,LH,255,cv2.THRESH_BINARY)
-    _,g = cv2.threshold(g,LS,255,cv2.THRESH_BINARY_INV)
+    _,r = cv2.threshold(r,253,255,cv2.THRESH_BINARY)
     
-    img, contours, hierarchy = cv2.findContours(r, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
-    print(contours)
-    if len(contours) != 0:
-        output = r.copy()
-        cv2.drawContours(frame, contours, -1, (0,255,0), 2)
-        c = max(contours, key = cv2.contourArea)
-        x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-         
-    cv2.imshow('r',r)
+    connectivity = 8  
+    # Perform the operation
+    output = cv2.connectedComponentsWithStats(r, connectivity, cv2.CV_32S)
+    # Get the results
+    # The first cell is the number of labels
+    num_labels = output[0]
+    # The second cell is the label matrix
+    labels = output[1]
+    # The third cell is the stat matrix
+    stats = output[2]
+    # The fourth cell is the centroid matrix
+    centroids = output[3]
+    
+    largest_area = stats[1,cv2.CC_STAT_AREA]
+    largest_label = 0
+    loop = np.unique(labels)
+    
+    #############################CCl part
+    largest_area = 0
+
+    mask = np.zeros_like(r)   
+
+    for i in loop[1:]:
+        
+        area = stats[i,cv2.CC_STAT_AREA]
+        if area > largest_area:
+            largest_area = area;
+            largest_label = i;
+            
+        mask[labels == largest_label] = i
+            
+        
+        
+    _,mask = cv2.threshold(mask,largest_label,255,cv2.THRESH_BINARY)
+    r = cv2.bitwise_and(r,r,mask = mask)        
+    w = stats[largest_label,cv2.CC_STAT_WIDTH]
+    h = stats[largest_label,cv2.CC_STAT_HEIGHT]
+    c = centroids[largest_label,:]
+    sx = int(centroids[largest_label,0] - w/2)
+    sy = int(centroids[largest_label,1] - h/2)
+    ex = int(centroids[largest_label,0] + w/2)
+    ey = int(centroids[largest_label,1] + h/2)
+    cv2.rectangle(frame,(sx,sy),(ex,ey),(255,255,0),2)
+
+    
+    
+    
+    
+    cv2.imshow('red',r)
     cv2.imshow('res',frame)
     
     
     
-     
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-    
-            
-
 
 cv2.destroyAllWindows()
-cap.release()
+    
+    
